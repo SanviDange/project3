0c7dd0769f87530dd7cf99384a7478aceafbfac4
SanviDange <dangs529@newschool.edu>
2026-04-14 19:06:54 -0400

    26-14-0.6

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function setup() {
   // Start detecting!
   faceMesh.detectStart(video, gotFaces);
 }
-
 function draw() {
   // Draw the video to the canvas
   image(video, 0, 0, width, height);
@@ -35,4 +34,42 @@ function draw() {
 
 function gotFaces(results) {
   faces = results;
+}
+
+//-----DOG API-----//
+
+let dogImage; // Variable to store the dog's picture
+
+function getDogDoppelganger() {
+  let url = "https://dog.ceo/api/breeds/image/random";
+  
+  // Use fetch to get data from the API
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // data.message is the URL of the random dog image
+      dogImage = loadImage(data.message); 
+      console.log("New buddy found: " + data.message);
+    });
+}
+
+function gotFaces(results) {
+  // If we see a face and haven't fetched a dog yet
+  if (faces.length === 0 && results.length > 0) {
+    getDogMatch(); // This calls your function that fills the HTML container
+  }
+  faces = results;
+}
+
+function getDogMatch() {
+  let url = "https://dog.ceo/api/breeds/image/random";
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let container = document.getElementById('dog-image-container');
+      
+      // 2. Create an image element and set the source
+      container.innerHTML = `<img src="${data.message}" alt="Dog Buddy">`;
+    });
 }
